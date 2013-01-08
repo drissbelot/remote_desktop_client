02489c69db2478d2c72ba0be4e0b28466e0cb22c
Jake Varley <jvarley@jvarley-Satellite-A505.(none)>
2013-01-08 15:03:20 -0800

    jenkins

--- src/com/remotecontrol/computercontroller/MainActivity.java
+++ src/com/remotecontrol/computercontroller/MainActivity.java
@@ -19,7 +19,6 @@ public class MainActivity extends Activity {
 	private SeekBar volumeSeekBar;
 	private Button rightClickButton;
 	private Button leftClickButton;
-	
 	private HttpController httpController;
 	private LinearLayout mousePad;
 
